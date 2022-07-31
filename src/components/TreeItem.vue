a00ed6e27e98a97234631ee83f6c2af45edb60b4
Alireza Shahamiri <shahamiri.alireza81@gmail.com>
2022-07-31 18:16:24 +0430

    feat(TreeItem): Adds Checkbox to TreeItem

--- src/components/TreeItem.vue
+++ src/components/TreeItem.vue
@@ -1,5 +1,6 @@
 <template>
 	<li class="tree-item">
+		<Checkbox :value="checkboxState" @click="checkboxClicked" />
 		<span @click="toggleCollapse">{{ data.name }}</span>
 		<ul class="tree-list" v-if="data.children" ref="treeList">
 			<TreeItem v-for="item in data.children" :key="item.name" :data="item" />
@@ -7,18 +8,73 @@
 	</li>
 </template>
 <script>
+	import Checkbox from './Checkbox.vue'
 	export default {
 		name: 'TreeItem',
 		components: {
+			Checkbox
 		},
 		props: {
 			data: {},
 			options: { type: Object }
 		},
+		data() {
+			return {
+				state: { isChecked: false, isHalfChecked: false },
+				checkboxState: 'unchecked',
+			}
+		},
 		methods: {
 			toggleCollapse() {
 				if (this.data.children)
 					this.$refs.treeList.classList.toggle('collapse')
+			},
+			updateCheckboxState() {
+				if (this.state.isChecked) {
+					return 'checked'
+				}
+				else if (this.state.isChecked === false) {
+					if (this.state.isHalfChecked) {
+						return 'halfChecked'
+					}
+					else {
+						return 'unchecked'
+					}
+				}
+			},
+			checkboxClicked() {
+				const rec = (item, state) => {
+					// if (!item.children) return
+					item.children.forEach(child => {
+						child.state = state
+						if (child.children) {
+							rec(child, state)
+						}
+					})
+					return item
+				}
+				// console.log(this.data)
+				// return
+				if (this.state.isChecked) {
+					this.state.isChecked = false
+				}
+				else {
+					this.state.isChecked = true
+					// this.data.children.map(child => child.state.isChecked = true)
+				}
+				if (this.data.children) {
+					this.data = rec(this.data, this.state)
+					// this.data.state = this.state
+				}
+				// this.state.isHalfChecked = false
+				// this.data.state = this.state
+
+				this.checkboxState = this.updateCheckboxState()
+				// console.log(this.updateCheckboxState())
+			},
+			shareNewState(state) {
+				if (!this.data.children) return
+
 			}
 		}
 	}
