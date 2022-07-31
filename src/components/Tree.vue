c7f2d1d03e98033988550589885546e03f0fe7b0
Alireza Shahamiri <shahamiri.alireza81@gmail.com>
2022-07-31 10:46:58 +0430

    feat(Tree): Adds options and Settings to Tree component

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -1,31 +1,47 @@
 <template>
-    <ul class="main-tree">
-      <TreeItem v-for="item in data" :key="item.name" :data="item" />
-    </ul>
+	<ul class="main-tree" :dir="settings.treeDirection">
+		<TreeItem v-for="item in data" :key="item.name" :data="item" />
+	</ul>
 </template>
 
 <script>
-import TreeItem from './TreeItem.vue'
+	import TreeItem from './TreeItem.vue'
+	import _ from 'lodash'
+
 	export default {
 		name: 'Tree',
-    components:{
-      TreeItem,
-    },
+		components: {
+			TreeItem,
+		},
 		props: {
 			data: { type: Array, default: [] },
 			options: { type: Object }
 		},
+		watch:{
+			options:{
+				deep:true,
+				immediate:true,
+				handler(val){
+					this.settings = _.merge(this.settings, val)
+				}
+			}
+		},
+		data() {
+			return {
+				settings: {
+					treeDirection: 'rtl', // ltr | rtl
+				}
+			}
+		},
 	}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 	ul {
 		list-style-type: none;
-		/* padding: 0; */
 	}
 	li {
-		/* display: inline-block; */
+		list-style: none;
 		margin: 10px 10px;
 	}
 	a {
