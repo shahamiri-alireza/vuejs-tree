9fd91361c84c73a2a3042c9872c80900fbdd06e7
Alireza Shahamiri <shahamiri.alireza81@gmail.com>
2022-09-06 16:31:09 +0430

    feat(Tree): Adds @change and some methods

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -1,6 +1,6 @@
 <template>
 	<ul class="main-tree" :dir="settings.treeDirection">
-		<TreeItem v-for="item in modelValue" :checkboxOptions="settings.checkboxOptions" :fields="settings.customFields" :key="item[settings.customFields.text]" :modelValue="item" @update:modelValue="item = $event" />
+		<TreeItem v-for="item in modelValue" :checkboxOptions="settings.checkboxOptions" :fields="settings.customFields" :key="item[settings.customFields.text]" :modelValue="item" @update:modelValue="item" @change="val => emitChanges(val)" />
 	</ul>
 </template>
 
@@ -17,6 +17,7 @@
 			modelValue: { type: Array, default: [] },
 			options: { type: Object }
 		},
+		emits: ['change'],
 		watch: {
 			options: {
 				deep: true,
@@ -24,12 +25,15 @@
 				handler(val) {
 					this.settings = _.merge(this.settings, val)
 				}
-			}
+			},
 		},
 		created() {
 			this.treeInit()
 		},
 		methods: {
+			emitChanges(val) {
+				this.$emit('change', val)
+			},
 			treeInit() {
 				this.addOrCheckStateFieldtoData(this.modelValue, false)
 			},
@@ -41,24 +45,68 @@
 					else if (!dataList[i][this.settings.customFields.state]) {
 						dataList[i][this.settings.customFields.state] = { isChecked: false }
 					}
+					else {
+						if (dataList[i][this.settings.customFields.children]) {
+							const chilrenState = this.getLastLevelItems(dataList[i][this.settings.customFields.children]).every(child => child[this.settings.customFields.state].isChecked === true)
+							dataList[i][this.settings.customFields.state] = { isChecked: chilrenState }
+						}
+					}
 
 
 					if (dataList[i][this.settings.customFields.children]?.length > 0)
 						this.addOrCheckStateFieldtoData(dataList[i][this.settings.customFields.children], dataList[i][this.settings.customFields.state].isChecked)
 				}
-			}
+			},
+			getLastLevelItems(items) {
+				let lastLevelItems = []
+
+				const getComputeds = (item) => {
+					if (item[this.settings.customFields.children]?.length) {
+						item[this.settings.customFields.children].forEach(child => getComputeds(child))
+					} else {
+						lastLevelItems.push(item)
+					}
+				}
+
+				for (let i = 0; i < items.length; i++) {
+					getComputeds(items[i])
+				}
+
+				return lastLevelItems
+			},
+			getLastLevelChecked() {
+				let result = _.cloneDeep(this.modelValue)
+
+				let computed = []
+
+				const getComputeds = (item) => {
+					if (item[this.settings.customFields.children]?.length) {
+						item[this.settings.customFields.children].forEach(child => getComputeds(child))
+					} else {
+						if (item[this.settings.customFields.state].isChecked) {
+							computed.push(item)
+						}
+					}
+				}
+
+				for (let i = 0; i < result.length; i++) {
+					getComputeds(result[i])
+				}
+
+				return computed
+			},
 		},
 		data() {
 			return {
 				settings: {
 					treeDirection: 'rtl', // ltr | rtl
 					customFields: {
-						text: 'esm',
+						text: 'text',
 						children: 'children',
 						state: 'state'
 					},
 					checkboxOptions: {
-						checkedColor: '#4646ff',
+						checkedColor: '#466eb5',
 						unCheckedColor: 'white'
 					}
 				}
@@ -67,13 +115,13 @@
 	}
 </script>
 
-<style>
+<style scoped>
 	ul {
 		list-style-type: none;
 	}
 	li {
 		list-style: none;
-		margin: 10px 10px;
+		margin: 0px 10px;
 	}
 	a {
 		color: #42b983;
